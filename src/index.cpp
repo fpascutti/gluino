5ddbcb42c8924b6f62777df9ded2ef0adf943b74
Franck Pascutti <franck.pascutti@thomsonreuters.com>
2017-03-09 14:21:05 +0100

    Added automatic formatting using 'Google' style for CPP files in VSCode.

--- src/index.cpp
+++ src/index.cpp
@@ -1,17 +1,14 @@
 #include <node.h>
 
-namespace gluino
-{
+namespace gluino {
 
-void Hello(const v8::FunctionCallbackInfo<v8::Value> &args)
-{
-    auto *isolate = args.GetIsolate();
-    args.GetReturnValue().Set(v8::String::NewFromUtf8(isolate, "world!"));
+void Hello(const v8::FunctionCallbackInfo<v8::Value>& args) {
+  auto* isolate = args.GetIsolate();
+  args.GetReturnValue().Set(v8::String::NewFromUtf8(isolate, "world!"));
 }
 
-void init(v8::Local<v8::Object> exports)
-{
-    NODE_SET_METHOD(exports, "hello", Hello);
+void init(v8::Local<v8::Object> exports) {
+  NODE_SET_METHOD(exports, "hello", Hello);
 }
 
 NODE_MODULE(GLUINO_MODULE_NAME, init)
